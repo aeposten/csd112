3642a3aa612121887ca8e09794db88a6c6730d22
Amy <86963506+aeposten@users.noreply.github.com>
2026-01-16 10:59:49 -0800

    Fixed button functionality, commented and organized CSS

--- assignments/assignment-1/script.js
+++ assignments/assignment-1/script.js
@@ -1,12 +1,20 @@
 // Variables for DOM elements
 const cssToggleButton = document.getElementById('css-toggle');
-
+const buttonText = cssToggleButton.textContent;
 // Event Listner for CSS toggle
 cssToggleButton.addEventListener('click', toggleCss);
 
 // Toggles CSS on and off by disablind/enabling stylesheet
-function toggleCss(){
+function toggleCss() {
     const styleSheet = document.querySelector('link[rel="stylesheet"]');
 
     styleSheet.disabled = !styleSheet.disabled;
+
+    if (styleSheet.disabled) {
+        cssToggleButton.textContent = "Enable Magic";
+        cssToggleButton.setAttribute('aria-pressed', 'true');
+    } else {
+        cssToggleButton.textContent = "Disable Magic";
+        cssToggleButton.setAttribute('aria-pressed', 'false');
+    }
 }
